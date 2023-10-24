8ba35d2fde277704746bbbaa261fe197a2ad24f9
Tuomo Ahonen <tuomo.ahonen@live.fi>
2023-10-24 15:39:00 +0300

    added jobs for build and tests

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
+	// you can also do this on eslintrs.js rules by using 'no-restricted-syntax' and set 'no-console': 'off'.
+	// eslint-disable-next-line no-console
 	console.log('server started on port 5000');
 });
+
