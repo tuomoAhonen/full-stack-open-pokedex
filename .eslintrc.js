f7b207f33921b0a4b786a88b93c5bd57573423fe
Tuomo Ahonen <tuomo.ahonen@live.fi>
2023-10-24 15:31:33 +0300

    edited eslint rules again

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,14 @@ module.exports = {
 		'no-trailing-spaces': 'error',
 		'object-curly-spacing': ['error', 'never'],
 		'arrow-spacing': ['error', {before: true, after: true}],
-		//'no-console': 'error',
+		'no-console': 'off' /*'error'*/,
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+				message: 'Unexpected property on console object was called',
+			},
+		],
 		'react/prop-types': 0,
 	},
 	settings: {
