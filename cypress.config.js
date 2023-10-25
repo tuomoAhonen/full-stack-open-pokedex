f2ecdc80fa69230c59580c4fceadd5accb1974fe
Tuomo Ahonen <tuomo.ahonen@live.fi>
2023-10-25 14:57:17 +0300

    testing cypress things part3

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const {defineConfig} = require('cypress');
+const webpackConfig = require('./webpack.config');
+
+module.exports = defineConfig({
+	component: {
+		devServer: {
+			framework: 'react',
+			bundler: 'webpack',
+			// optionally pass in webpack config
+			webpackConfig,
+			// or a function - the result is merged with any
+			// webpack.config that is found
+			/*webpackConfig: async () => {
+				// ... do things ...
+				const modifiedConfig = await injectCustomConfig(baseConfig);
+				return modifiedConfig;
+			},*/
+		},
+	},
+});
