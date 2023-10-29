48dc4fcd1c9afbd58399497bef056f95017c85d4
Tuomo Ahonen <tuomo.ahonen@live.fi>
2023-10-29 12:37:55 +0200

    part b done, more fixing

--- cypress/e2e/Helpers.js
+++ cypress/e2e/Helpers.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const getIvysaurObject = () => {
+const getIvysaurObject = async () => {
 	//let ivysaur;
 
 	return axios
 		.get('https://pokeapi.co/api/v2/pokemon/ivysaur')
-		.then((response) => (ivysaur = response.data))
+		.then((response) => response.data)
 		.catch((e) => console.log(e));
 
 	//console.log(ivysaur);
